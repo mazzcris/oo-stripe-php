4171ed8e30acb0fa79c082aa6f73455c73517461
Cristiano Mazzarini <cristiano.mazzarini@gmail.com>
2024-11-09 13:11:19 +0100

    Add DynamicGetterTrait

--- src/DynamicGetterTrait.php
+++ src/DynamicGetterTrait.php
@@ -4,5 +4,33 @@ namespace Mazzcris\OoStripePhp;
 
 trait DynamicGetterTrait
 {
+    public function __call($name, $arguments)
+    {
+        if (str_starts_with($name, 'get')) {
+            $property = $this->camelToSnake(lcfirst(substr($name, 3)));
 
+            if (!isset($this->$property) && $this->$property !== null) {
+                throw new \BadMethodCallException("Method {$name} does not exist");
+            }
+
+            return $this->transformProperty($this->$property);
+        }
+
+        throw new \BadMethodCallException("Method {$name} does not exist");
+    }
+
+    private function camelToSnake($string): string
+    {
+        return strtolower(preg_replace('/[A-Z]/', '_$0', $string));
+    }
+
+    private function transformProperty($value)
+    {
+        // Convert Unix timestamp to DateTime
+        if (is_int($value) && strlen((string)$value) === 10) {
+            return (new \DateTime())->setTimestamp($value);
+        }
+
+        return $value;
+    }
 }
